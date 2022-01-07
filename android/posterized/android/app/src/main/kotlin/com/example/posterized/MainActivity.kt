35c8e2343f1ee26b9b3f81aee99df08a6089cafa
Fathan Radhiyan <fathanradhiyan@gmail.com>
2022-01-07 15:34:49 +0700

    set only portrait

---
+++ android/posterized/android/app/src/main/kotlin/com/example/posterized/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.posterized
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:posterized/const/color.dart';
 import 'package:posterized/pages/input_page.dart';
 import 'package:posterized/pages/opening_page.dart';
@@ -8,8 +9,19 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
